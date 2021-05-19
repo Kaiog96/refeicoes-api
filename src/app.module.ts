754314342f0e2dcf2a6f1d44a3296b25c3b5e2a2
Kaio Garcia <contatokaiogarcia@gmail.com>
2021-05-19 16:24:22 -0300

    removido importação não utilizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiService } from './api/api.service';
-import { MealController } from './meal/meal.controller';
-import { MealService } from './meal/meal.service';
-
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, MealController],
-  providers: [ApiService, AppService, MealService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
